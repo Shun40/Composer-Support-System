950b96e08021c538b4b0d2ae210d52bbcd34c9ac
aike <aaiikkee@gmail.com>
2016-07-21 05:52:31 +0900

    add mixer interface skelton

--- src/jp/hci_lab/muplat/Mixer.java
+++ src/jp/hci_lab/muplat/Mixer.java
@@ -2,4 +2,23 @@ package jp.hci_lab.muplat;
 
 public class Mixer {
 
+	public void SetMasterLevel(int level) {
+		
+	}
+		
+	public void SetLevel(int tr, int level) {
+		
+	}
+
+	public void SetPan(int tr, int pan) {
+		
+	}
+	
+	public void SetSolo(int tr, boolean solo) {
+		
+	}
+
+	public void SetMute(int tr, boolean mute) {
+		
+	}
 }
