15d4be8e483a2cc045f931a9901a26a0c4364ff6
aike <aaiikkee@gmail.com>
2016-08-14 04:51:26 +0900

    add around voicing

--- src/jp/hci_lab/muplat/ChordPlayer.java
+++ src/jp/hci_lab/muplat/ChordPlayer.java
@@ -4,54 +4,59 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class ChordPlayer {
+	boolean baseEnable = true;
+	int bassCenter = 36;
+	boolean chordAround = true;
+	int chordCenter = 66;
+	
+	HashMap<String, Integer> noteId = new HashMap<String, Integer>();
 	HashMap<String, int[]> chordnotes = new HashMap<String, int[]>();
 	DAW daw_;
 	
 	ChordPlayer(DAW daw) {
+		noteId.put("C",  0);
+		noteId.put("C#", 1);
+		noteId.put("Db", 1);
+		noteId.put("D",  2);
+		noteId.put("D#", 3);
+		noteId.put("Eb", 3);
+		noteId.put("E",  4);
+		noteId.put("F",  5);
+		noteId.put("F#", 6);
+		noteId.put("Gb", 6);
+		noteId.put("G",  7);
+		noteId.put("G#", 8);
+		noteId.put("Ab", 8);
+		noteId.put("A",  9);
+		noteId.put("A#", 10);
+		noteId.put("Bb", 10);
+		noteId.put("B",  11);		
+		
 		daw_ = daw;
 
 		DefineChord("C", new int[] {60, 64, 67});
-		DefineChord("D", Shift("C", 2));
-		DefineChord("E", Shift("C", 4));
-		DefineChord("F", Shift("C", 5));
-		DefineChord("G", Shift("C", 7));
-		DefineChord("A", Shift("C", 9));
-		DefineChord("B", Shift("C", 11));
+		ShiftAll("");
 
 		DefineChord("Cm", new int[] {60, 63, 67});
-		DefineChord("Dm", Shift("Cm", 2));
-		DefineChord("Em", Shift("Cm", 4));
-		DefineChord("Fm", Shift("Cm", 5));
-		DefineChord("Gm", Shift("Cm", 7));
-		DefineChord("Am", Shift("Cm", 9));
-		DefineChord("Bm", Shift("Cm", 11));
+		ShiftAll("m");
 
 		DefineChord("C7", new int[] {60, 64, 67, 70});
-		DefineChord("D7", Shift("C7", 2));
-		DefineChord("E7", Shift("C7", 4));
-		DefineChord("F7", Shift("C7", 5));
-		DefineChord("G7", Shift("C7", 7));
-		DefineChord("A7", Shift("C7", 9));
-		DefineChord("B7", Shift("C7", 11));
+		ShiftAll("7");
 
 		DefineChord("CM7", new int[] {60, 64, 67, 71});
-		DefineChord("DM7", Shift("CM7", 2));
-		DefineChord("EM7", Shift("CM7", 4));
-		DefineChord("FM7", Shift("CM7", 5));
-		DefineChord("GM7", Shift("CM7", 7));
-		DefineChord("AM7", Shift("CM7", 9));
-		DefineChord("BM7", Shift("CM7", 11));
+		ShiftAll("M7");
 
 		DefineChord("Cm7", new int[] {60, 63, 67, 70});
-		DefineChord("Dm7", Shift("Cm7", 2));
-		DefineChord("Em7", Shift("Cm7", 4));
-		DefineChord("Fm7", Shift("Cm7", 5));
-		DefineChord("Gm7", Shift("Cm7", 7));
-		DefineChord("Am7", Shift("Cm7", 9));
-		DefineChord("Bm7", Shift("Cm7", 11));
+		ShiftAll("m7");
 
+		DefineChord("Csus4", new int[] {60, 65, 67});
+		ShiftAll("sus4");
+		
 		DefineChord("Bmb5", new int[] {71, 74, 77});
+		ShiftAll("B", "mb5");
+		
 		DefineChord("Bm7b5", new int[]{71, 74, 77, 81});
+		ShiftAll("B", "m7b5");
 
 	}
 	
@@ -81,15 +86,44 @@ public class ChordPlayer {
 		
 	}
 
+	public void BassOn(boolean b) {
+		baseEnable = b;
+	}
+	
+	int aroundNote(int center, int noteNo) {
+		int noteId = noteNo % 12;
+		int offset = noteId - (center % 12);
+		if (offset < -6) {
+			offset += 12;
+		}
+		return center + offset;
+	}
+	
 	public void PlayChord(String s) {
 		try {
-			int notes[] = chordnotes.get(s);
+			int root = 0;
+			int notes[] = chordnotes.get(s).clone();
+
+			if (chordAround) {
+				for (int i = 0; i < notes.length; i++) {
+					notes[i] = aroundNote(chordCenter, notes[i]);
+				}
+			}
+
 			if (notes != null) {
+				if (baseEnable) {
+					root = aroundNote(bassCenter, notes[0]);
+					daw_.NoteOn(root, 100);
+				}
+				
 				for (int i = 0; i < notes.length; i++) {
 					daw_.NoteOn(notes[i], 100);
 				}
 			}
 			Thread.sleep(1500);
+			if (baseEnable) {
+				daw_.NoteOff(root);
+			}
 			if (notes != null) {
 				for (int i = 0; i < notes.length; i++) {
 					daw_.NoteOff(notes[i]);
@@ -105,6 +139,33 @@ public class ChordPlayer {
 		chordnotes.put(name, notes.clone());
 	}
 
+	void ShiftAll(String type) {
+		ShiftAll("C", type);
+	}
+
+	void ShiftAll(String base, String type) {
+		int offset = noteId.get(base);
+		String chord = base + type;
+
+		DefineChord("C"  + type, Shift(chord,  0 - offset));
+		DefineChord("C#" + type, Shift(chord,  1 - offset));
+		DefineChord("Db" + type, Shift(chord,  1 - offset));
+		DefineChord("D"  + type, Shift(chord,  2 - offset));
+		DefineChord("D#" + type, Shift(chord,  3 - offset));
+		DefineChord("Eb" + type, Shift(chord,  3 - offset));
+		DefineChord("E"  + type, Shift(chord,  4 - offset));
+		DefineChord("F"  + type, Shift(chord,  5 - offset));
+		DefineChord("F#" + type, Shift(chord,  6 - offset));
+		DefineChord("Gb" + type, Shift(chord,  6 - offset));
+		DefineChord("G"  + type, Shift(chord,  7 - offset));
+		DefineChord("G#" + type, Shift(chord,  8 - offset));
+		DefineChord("Ab" + type, Shift(chord,  8 - offset));
+		DefineChord("A"  + type, Shift(chord,  9 - offset));
+		DefineChord("A#" + type, Shift(chord, 10 - offset));
+		DefineChord("Bb" + type, Shift(chord, 10 - offset));
+		DefineChord("B"  + type, Shift(chord, 11 - offset));		
+	}
+	
 	int[] Shift(String base, int offset) {
 		int notes[] = chordnotes.get(base).clone();
 		for (int i = 0; i < notes.length; i++) {
